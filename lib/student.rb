e83b24de8721c1bb09cf455bb87dc963f2501663
Brittany Javalera <brittjavalera@gmail.com>
2019-07-27 22:12:36 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,11 +5,16 @@ class Student
   @@all = []
 
   def initialize(student_hash)
+    @name = student_hash[:name]
+    @location = student_hash[:location]
     @@all << self
+    
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+  end
   end
 
   def add_student_attributes(attributes_hash)
@@ -17,7 +22,7 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
 
