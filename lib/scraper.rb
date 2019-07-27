c53dfc93d55b7c3237f09ce81f88e0afe546373c
David Foote <david.a.foote@gmail.com>
2019-07-27 05:10:25 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,38 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open('fixtures/student-site/index.html')
+    doc = Nokogiri::HTML(html)
+    #binding.pry
+    
+    scraped_students = []
+    
+    students = doc.css(".student-card a")
+    students.each do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile_url = student.css("a").attribute("href").value
+      hash = {:name => name,
+      :location => location,
+      :profile_url => profile_url
+    }
+    scraped_students << hash
+  end
+    scraped_students
+    
+    
+      
+#       scrapedStudents= []   ``````make empty array
+#       students = doc.css("div .roster-cards-container")
+#       students.each do |student|
+#         ```scrape  the desired texts and make them into a hash
+#         ```push the hash into scrapedStudents
+#     end
+#     return scrapedStudents
     
   end
 
