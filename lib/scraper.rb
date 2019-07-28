0a4dbb87f0c41dcce85a92d2df008a88b51ed3e3
David Foote <david.a.foote@gmail.com>
2019-07-28 03:47:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,15 +7,14 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open('fixtures/student-site/index.html')
     doc = Nokogiri::HTML(html)
-    #binding.pry
     
     scraped_students = []
     
-    students = doc.css(".student-card a")
+    students = doc.css(".student-card")
     students.each do |student|
       name = student.css(".student-name").text
       location = student.css(".student-location").text
-      profile_url = student.css("a").attribute("href").value
+      profile_url = student.css("a").attr("href").text
       hash = {:name => name,
       :location => location,
       :profile_url => profile_url
@@ -23,22 +22,32 @@ class Scraper
     scraped_students << hash
   end
     scraped_students
-    
-    
-      
-#       scrapedStudents= []   ``````make empty array
-#       students = doc.css("div .roster-cards-container")
-#       students.each do |student|
-#         ```scrape  the desired texts and make them into a hash
-#         ```push the hash into scrapedStudents
-#     end
-#     return scrapedStudents
-    
   end
 
   def self.scrape_profile_page(profile_url)
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
     
+    student_profiles = {}
+   
+    social_link = doc.css(".vitals-container .social-icon-container a")
+    
+    social_link.each do |element|
+      if element.attr("href").include?("twitter")
+        student_profiles[:twitter] = element.attr('href')
+      elsif element.attr("href").include?("linkedin")
+        student_profiles[:linkedin] = element.attr("href")
+      elsif element.attr("href").include?("github")
+        student_profiles[:github] = element.attr("href")
+      elsif element.attr("href").include?("com/")
+      student_profiles[:blog] = element.attr("href")
+      end
+    end
+    
+    student_profiles[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
+    student_profiles[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
+    
+    student_profiles
   end
-
 end
 
